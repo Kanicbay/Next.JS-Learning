9a5770145346ed2d06cf6aa2f356f3572e0405c6
Brian Coyago <briancoyago@hotmail.com>
2025-12-06 19:55:55 -0500

    feat(nextjs): Linking and Navigation
    Use first a tag for navigation but using docs from nextjs use link is better than a in user experience and performance
    
    Brian Coyago

--- NextJS/NextJS 16 Full Stack Course/nextjs-16-tutorial-yt/app/contact/page.tsx
+++ NextJS/NextJS 16 Full Stack Course/nextjs-16-tutorial-yt/app/contact/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function ContactPage() {
     return (
         <div>
             <h1>Hello From the Contact Page</h1>
+            <Link href="/contact/email">Go to Email</Link >
         </div>
     );
 }
--- NextJS/NextJS 16 Full Stack Course/nextjs-16-tutorial-yt/app/page.tsx
+++ NextJS/NextJS 16 Full Stack Course/nextjs-16-tutorial-yt/app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h1>Hello from the index page</h1>
+      <Link href="/contact">Go to Contact</Link>
     </div>
   );
 }
