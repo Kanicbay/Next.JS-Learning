becbf3b3b5366f280960770f2b92a251987410eb
Brian Coyago <briancoyago@hotmail.com>
2025-12-08 16:52:16 -0500

    feat(nextjs): continue with auth pages
    Brian Coyago

--- NextJS/NextJS 16 Full Stack Course/nextjs-16-tutorial-yt/app/auth/sign-up/page.tsx
+++ NextJS/NextJS 16 Full Stack Course/nextjs-16-tutorial-yt/app/auth/sign-up/page.tsx
@@ -1,12 +1,30 @@
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { signUpSchema } from "@/app/schemas/auth";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
-export default function SignUpPage(){
+export default function SignUpPage() {
+
+    const form = useForm({
+        resolver: zodResolver(signUpSchema),
+        defaultValues: {
+            name: "",
+            email: "",
+            password: ""
+        }
+    });
+
     return (
         <Card>
             <CardHeader>
                 <CardTitle>Sign Up</CardTitle>
                 <CardDescription>Create an account to get started</CardDescription>
             </CardHeader>
+            <CardContent>
+                <form>
+
+                </form>
+            </CardContent>
         </Card>
     );
 }
